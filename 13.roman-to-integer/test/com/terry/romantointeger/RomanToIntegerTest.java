3eeba35068cd8489b1a073d382cb9625ae081e64
terryhy520 <terryhy520@gmail.com>
2019-09-21 21:05:51 +0800

    Add 20. Roman To Integer Src and Test codee

---
+++ 13.roman-to-integer/test/com/terry/romantointeger/RomanToIntegerTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019.  Terry All rights Reserved.
+ */
+
+package com.terry.romantointeger;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RomanToIntegerTest {
+    @Test
+    public void testRomanToInt01() {
+        String s = "IV";
+        RomanToInteger romanToInteger = new RomanToInteger();
+        assertEquals(4, romanToInteger.romanToInt(s));
+    }
+
+    @Test
+    public void testRomanToInt02() {
+        String s = "LVIII";
+        RomanToInteger romanToInteger = new RomanToInteger();
+        assertEquals(58, romanToInteger.romanToInt(s));
+    }
+
+    @Test
+    public void testRomanToInt03() {
+        String s = "MCMXCIV";
+        RomanToInteger romanToInteger = new RomanToInteger();
+        assertEquals(1994, romanToInteger.romanToInt(s));
+    }
+}
