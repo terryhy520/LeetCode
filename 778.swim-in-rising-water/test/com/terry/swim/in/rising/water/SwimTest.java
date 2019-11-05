8376b0e4c2c4c9432fd1917a1a6f659771ce3b46
terryhy520 <terryhy520@gmail.com>
2019-11-05 20:38:08 +0800

    add swim source code and test code

---
+++ 778.swim-in-rising-water/test/com/terry/swim/in/rising/water/SwimTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2019.  Terry All rights Reserved.
+ */
+
+package com.terry.swim.in.rising.water;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class SwimTest {
+
+    @Test
+    public void swimInWater01() {
+        int[][] grid = new int[][]{
+                {0, 1, 2, 3, 4},
+                {24, 23, 22, 21, 5},
+                {12, 13, 14, 15, 16},
+                {11, 17, 18, 19, 20},
+                {10, 9, 8, 7, 6}};
+        Swim swim = new Swim();
+        int result = swim.swimInWater(grid);
+        assertEquals(16, result);
+    }
+
+    @Test
+    public void swimInWater02() {
+        int[][] grid = new int[][]{
+                {0, 2},
+                {1, 3}};
+        Swim swim = new Swim();
+        int result = swim.swimInWater(grid);
+        assertEquals(3, result);
+    }
+}
