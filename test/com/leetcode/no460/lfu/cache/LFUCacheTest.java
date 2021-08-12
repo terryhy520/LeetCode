c301c666236268aadaddb7d2d7f79b206f18e13d
terryhy520 <terryhy520@qq.com>
2021-08-12 22:12:26 +0800

    lfu test

--- test/com/leetcode/no460/lfu/cache/LFUCacheTest.java
+++ test/com/leetcode/no460/lfu/cache/LFUCacheTest.java
@@ -1,5 +1,23 @@
 package com.leetcode.no460.lfu.cache;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LFUCacheTest {
 
+    @Test
+    public void LFUCache01() {
+        LFUCache lfuCache = new LFUCache(2);
+        lfuCache.put(1, 1);
+        lfuCache.put(2, 2);
+        assertEquals(1, lfuCache.get(1));
+        lfuCache.put(3, 3);
+        assertEquals(-1, lfuCache.get(2));
+        assertEquals(3, lfuCache.get(3));
+        lfuCache.put(4, 4);
+        assertEquals(-1, lfuCache.get(1));
+        assertEquals(3, lfuCache.get(3));
+        assertEquals(4, lfuCache.get(4));
+    }
 }
